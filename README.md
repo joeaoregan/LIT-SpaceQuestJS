0ced343cfd992627eb971db9fd979f466e47367b
joeaoregan <joe.oregan@gmail.com>
2025-06-27 15:23:50 +0100

    update dependencies, server, readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-[Space Quest Web App](https://spacequest.onrender.com/)
+See app on:
+
+- [Render](https://spacequest.onrender.com/ "See app on Render")
+- [GitHub Pages](https://joeaoregan.github.io/LIT-SpaceQuestJS/ "See app on GitHub Pages")
 
 ---
 
@@ -10,18 +13,20 @@ Space Quest game ported to JavaScript from fourth-year Platform Game Development
 
 ### Links:
 
-* [Play Web App (Work in progress)](https://spacequest.onrender.com/ "Play Heroku Web App (Work in progress)")
-* [Space Quest: Original Version Repo](https://github.com/joeaoregan/LIT-Yr4-PlatformGameDevelopment "Space Quest: Original Version Repo")
-* [Space Quest: YouTube Trailer](https://www.youtube.com/watch?feature=player_embedded&v=TVLTJW5FwQs "Space Quest: YouTube Trailer")
+- [Play Web App (Work in progress)](https://spacequest.onrender.com/ "Play Render Web App (Work in progress)")
+- [Space Quest: Original Version Repo](https://github.com/joeaoregan/LIT-Yr4-PlatformGameDevelopment "Space Quest: Original Version Repo")
+- [Space Quest: YouTube Trailer](https://www.youtube.com/watch?feature=player_embedded&v=TVLTJW5FwQs "Space Quest: YouTube Trailer")
 
 ---
 
 ### Screenshots:
 
 ![Space Game: Level 2](https://raw.githubusercontent.com/joeaoregan/LIT-Yr4-PlatformGameDevelopment/master/Screenshots/SpaceGame7Level2.jpg "Space Shooter: Level 2")
+
 ###### Space Quest: Level 2 (Original Version)
 
 ---
 
 ![Space Game: Level 3](https://raw.githubusercontent.com/joeaoregan/LIT-Yr4-PlatformGameDevelopment/master/Screenshots/SpaceGame12AsteroidsAndEnemies.jpg "Space Shooter: Level 3")
+
 ###### Space Quest: Level 3
--- server.js
+++ server.js
@@ -4,13 +4,13 @@
 	17/01/2020
 	Space Quest - Server
 */
-const express = require('express');
-socketio = require('socket.io');
+const express = require("express");
+socketio = require("socket.io");
 
 const port = process.env.PORT || 3000;
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 /*
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
 	console.log('addr: ' + add);
@@ -18,22 +18,42 @@ require('dns').lookup(require('os').hostname(), function (err, add, fam) {
 	console.log('fam: ' + fam);
 });
 */
+
+const os = require("os");
 console.log(
-	`   _________                         ________                          __   
-  /   _____/__________    ____  ____ \\_____  \\  __ __   ____   _______/  |_ 
-  \\_____  \\\\____ \\__  \\ _/ ___\\/ __ \\ /  / \\  \\|  |  \\_/ __ \\ /  ___/\\   __\\
-  /        \\  |_> > __ \\\\  \\__\\  ___//   \\_/.  \\  |  /\\  ___/ \\___ \\  |  |  
- /_______  /   __(____  /\\___  >___  >_____\\ \\_/____/  \\___  >____  > |__|  
-         \\/|__|       \\/     \\/    \\/       \\__>           \\/     \\/        `);
+  "Platform: " +
+    os.platform() +
+    " Architecture: " +
+    os.arch() +
+    " Hostname: " +
+    os.hostname()
+);
+const dns = require("dns");
 
-const os = require('os');
-console.log("Platform: " + os.platform() + " Architecture: " + os.arch() + " Hostname: " + os.hostname());
-const dns = require('dns');
+// let addr=dns.lookup(os.hostname(), function(err,add,fam){
+// 	console.log("Server running on " +add+":"+ port);
+// });
 
-let addr=dns.lookup(os.hostname(), function(err,add,fam){
-	console.log("Server running on " +add+":"+ port);
-});
-
-app.use(express.static('static'));
+app.use(express.static("docs"));
 
 server.listen(port);
+
+const red = "\x1b[31m";
+const white = "\x1b[37m";
+const magenta = "\x1b[35m";
+const cyan = "\x1b[36m";
+const reset = "\x1b[0m";
+
+console.log(
+  `${red}   _________ ${reset}                        ________                          __   
+  ${red}/   _____/__________    ____  ____ ${reset}\\_____  \\  __ __   ____   _______/  |_ 
+ ${red} \\_____  \\\\____ \\__  \\ _/ ___\\/ __ \\ ${reset}/  / \\  \\|  |  \\_/ __ \\ /  ___/\\   __\\
+ ${red} /        \\  |_> > __ \\\\  \\__\\  ___/${reset}/   \\_/.  \\  |  /\\  ___/ \\___ \\  |  |  
+${red} /_______  /   __(____  /\\___  >___  >${reset}_____\\ \\_/____/  \\___  >____  > |__|  
+${red}         \\/|__|       \\/     \\/    \\/${reset}       \\__>           \\/     \\/        `
+);
+
+console.log(
+  white + "Server running at " + magenta + "http://localhost:" + port + reset
+);
+console.log("Press " + cyan + "Ctrl+C" + reset + " to stop the server");
